154609c9aea8101017a2e8d8b31b18456ab61b2a
jony89 <ariel@emath.co.il>
2022-12-02 15:11:59 +0200

    chore: support topics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
 
 const languages = [{
   name: "English",
@@ -11,26 +12,54 @@ const languages = [{
   room: "https://easytalk.daily.co/Spanish"
 }]
 
+
+const topics = ["Warzone", "WorldCup 2022", "Entrepreneurship", "Politics", "History", "Innovation"]
+
 function App() {
+  const [step, setStep] = useState(0);
+  const [lang, setLang] = useState(languages[0]);
+  const [topic, setTopic] = useState(topics[0]);
+
   const onChangeLanguage = (lang) => {
+    setLang(lang);
+    setStep(1);
+  }
+
+  const onTopicChange = (topic) => {
     window.open(lang.room, "_blank")
   }
+
+  const startOverClick = () => {
+    setStep(0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="EasyTalk" />
+        <img src={logo} alt="EasyTalk" class="app-logo" />
         <p>
           with EasyTalk you can now study any language any time.
         </p>
-        <h2>Choose Language</h2>
-        <div className="flags">
+        {step === 0 && <h2>Choose Language</h2>}
+        {step === 1 && <h2>Choose Topic</h2>}
+        {step === 0 && <div className="flags">
           {languages.map(lang => {
-            return <div key={lang.name  } className="language-flag" onClick={() => onChangeLanguage(lang)}>
+            return <div key={lang.name} className="language-flag" onClick={() => onChangeLanguage(lang)}>
               <h3>{lang.name}</h3>
               <img src={lang.logo} alt={lang.name} />
             </div>
           })}
-        </div>
+        </div>}
+        {step === 1 && <div className="topics">
+          {topics.map(topic => {
+            return <div onClick={() => onTopicChange(topic)} className="topic">
+              <span>{topic}</span>
+            </div>
+          })}
+        </div>}
+        {step !== 0 && <div>
+          <button onClick={startOverClick} className='start-over-btn'>Start Over</button>
+        </div>}
       </header>
     </div>
   );
